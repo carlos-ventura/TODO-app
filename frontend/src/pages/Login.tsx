9232a79703c229b62f810edaa8bd66f3829bafaf
carlos-ventura <carlospinho_pt@hotmail.com>
2023-01-20 15:39:11 +0100

    Create simple Login page

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+interface IFormData {
+  username: string
+  password: string
+}
+
+const LoginPage = (): JSX.Element => {
+  const [formData, setFormData] = useState<IFormData>({
+    username: '',
+    password: ''
+  })
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    // add code here to send login data to server
+    console.log('username:', formData.username)
+    console.log('password:', formData.password)
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" value={formData.username} onChange={handleInputChange} />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+      <br/><br/>
+    </div>
+  )
+}
+
+export default LoginPage
