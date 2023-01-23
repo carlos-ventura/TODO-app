410047da8c39126e2694e614e3d1e57f281c2965
carlos-ventura <carlospinho_pt@hotmail.com>
2023-01-23 18:55:27 +0100

    Add all tasks API methods

--- frontend/src/pages/Todos.tsx
+++ frontend/src/pages/Todos.tsx
@@ -1,25 +1,56 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Todo from '../components/Todo'
 import TodoForm from '../components/TodoForm'
 import { ITodo } from '../types'
 
+const todosAPI = String(process.env.REACT_APP_API_URL) + 'tasks'
+
 const TodosPage = (): JSX.Element => {
-  const todos: ITodo[] = [
-    { text: 'Todo1 with a longer text just because', completed: false },
-    { text: 'Todo2', completed: true },
-    { text: 'Todo3', completed: false },
-    { text: 'Todo4', completed: true }
-  ]
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+  const loadTodos = async (): Promise<void> => {
+    const response = await axios.get(todosAPI)
+    setTodos(response.data)
+  }
+
+  const createTodo = async (todo: ITodo): Promise<void> => {
+    await axios.post(todosAPI, {
+      text: todo.text,
+      completed: todo.completed
+    })
+    await loadTodos()
+  }
+
+  const updateTodo = async (todo: ITodo): Promise<void> => {
+    await axios.put(`${todosAPI}/${todo.id}`, {
+      id: todo.id,
+      text: todo.text,
+      completed: todo.completed
+    })
+    await loadTodos()
+  }
+
+  const deleteTodo = async (todo: ITodo): Promise<void> => {
+    // Small bug here need to refresh page for text update
+    await axios.delete(`${todosAPI}/${todo.id}`)
+    await loadTodos()
+  }
+
+  useEffect(() => {
+    loadTodos()
+      .catch(console.error)
+  }, [])
 
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>TODO List</h1>
       <br/>
-      <TodoForm />
+      <TodoForm createTodo={createTodo} />
       <br/><br/>
       {todos.map((todo, index) => (
-        <Todo key={index} todo={todo} />
+        <Todo key={index} todo={todo} deleteTodo={deleteTodo} updateTodo={updateTodo} />
       ))}
       <br/><br/>
       <Link to="/">Home</Link>
