98a4a7452eb1f48857ae4a108ced8ab54eb9d534
carlos-ventura <carlospinho_pt@hotmail.com>
2023-01-23 18:54:25 +0100

    Update Todo to implement api methods

--- frontend/src/components/Todo.tsx
+++ frontend/src/components/Todo.tsx
@@ -3,16 +3,29 @@ import { ITodo } from '../types'
 
 interface TodoProps {
   todo: ITodo
+  updateTodo: (todo: ITodo) => Promise<void>
+  deleteTodo: (todo: ITodo) => Promise<void>
 }
 
-const Todo = ({ todo }: TodoProps): JSX.Element => {
+const Todo = ({ todo, updateTodo, deleteTodo }: TodoProps): JSX.Element => {
+  const handleDelete = async (): Promise<void> => {
+    await deleteTodo(todo)
+  }
+
+  const handleTextUpdate = async (e: React.ChangeEvent<HTMLInputElement>): Promise<void> => {
+    await updateTodo({ ...todo, text: e.target.value })
+  }
+
+  const handleCompleteUpdate = async (): Promise<void> => {
+    await updateTodo({ ...todo, completed: !todo.completed })
+  }
+
   return (
     <div style={{ display: 'flex', padding: '10px' }}>
-      <span style={{ textDecoration: todo.completed ? 'line-through' : '' }}>{todo.text}</span>
+      <input onChange={(e) => { void handleTextUpdate(e) }} defaultValue={todo.text} type="text" disabled={todo.completed} />
       <div style={{ display: 'flex', paddingLeft: '30px' }}>
-        <button>Edit</button>
-        <button>✓</button>
-        <button>✕</button>
+        <button onClick={() => { void handleCompleteUpdate() }}>✓</button>
+        <button onClick={() => { void handleDelete() } }>✕</button>
       </div>
     </div>
   )
