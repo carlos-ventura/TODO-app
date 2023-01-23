ad622fa37590ac134546ba14f82688fd43af4eb5
carlos-ventura <carlospinho_pt@hotmail.com>
2023-01-23 18:44:25 +0100

    Setup connection string and database

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,6 +1,13 @@
 using backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Add services to the container.
+
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(connectionString));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
