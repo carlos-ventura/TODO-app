bb1f094fe1889d4c4325668b22d55e2f632f7018
carlos-ventura <carlospinho_pt@hotmail.com>
2023-01-24 15:52:18 +0100

    final tweaks on the backend

--- backend/backend/Models/Todo.cs
+++ backend/backend/Models/Todo.cs
@@ -1,7 +1,7 @@
 ﻿namespace backend.Models {
     public class Todo {
         public int Id { get; set; }
-        public string Text { get; set; }
+        public string Text { get; set; } = string.Empty;
         public bool Completed { get; set; }
     }
 }
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -55,4 +55,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run("https://localhost:7160");
