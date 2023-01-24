676458b70cb1086651a8e50d75456e8b7ba78874
carlos-ventura <carlospinho_pt@hotmail.com>
2023-01-24 13:11:47 +0100

    Create Token controller

---
+++ backend/backend/Controllers/TokenController.cs
@@ -0,0 +1,26 @@
+﻿using backend.Models;
+using backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers {
+    [Route("/auth")]
+    [ApiController]
+    public class TokenController : ControllerBase {
+
+        private readonly IJwtTokenManager _tokenManager;
+
+        public TokenController(IJwtTokenManager jwtTokenManager) {
+            _tokenManager = jwtTokenManager;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Authenticate([FromBody] User user) {
+            var token = _tokenManager.Authenticate(user.Username);
+            if(string.IsNullOrEmpty(token))
+                return Unauthorized();
+            return Ok(token);
+        }
+    }
+}
