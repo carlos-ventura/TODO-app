d566a2c72b832998c04ac336abb7fdab712a051c
carlos-ventura <carlospinho_pt@hotmail.com>
2023-01-24 13:08:13 +0100

    Create static users

---
+++ backend/backend/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using backend.Models;
+
+namespace backend.Repositories {
+    public static class UserRepository {
+
+        static User[] users = new User[]{
+                 (new User { Username = "root" }),
+                 (new User { Username= "admin"}),
+                 (new User { Username= "superuser" })
+           };
+        public static User? Get(string username) {
+            return users.FirstOrDefault(x => x.Username.ToLower() == username.ToLower());
+        }
+    }
+}
+
