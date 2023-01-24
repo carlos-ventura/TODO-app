6b5608aebe20acf56c4f0cc64999e0751d0fbeca
carlos-ventura <carlospinho_pt@hotmail.com>
2023-01-24 13:11:02 +0100

    Create token manager class

---
+++ backend/backend/Services/JwtTokenManager.cs
@@ -0,0 +1,40 @@
+﻿using backend.Repositories;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace backend.Services {
+    public class JwtTokenManager : IJwtTokenManager {
+
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenManager(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+        public string Authenticate(string username) {
+            var user = UserRepository.Get(username);
+
+            if (user == null) {
+                return null;
+            }
+
+            var key = _configuration.GetValue<string>("JwtConfig:Key");
+            var keyBytes = Encoding.ASCII.GetBytes(key);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var tokenDescriptor = new SecurityTokenDescriptor() {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.NameIdentifier, username)
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
